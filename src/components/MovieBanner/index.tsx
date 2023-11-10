a68cb99b2e21d75d436a67823c9bda4958138834
Antonio Sabino <aslimajr@outlook.com>
2023-11-09 22:44:49 -0300

    feat: utilização do swiper no banner

--- src/components/MovieBanner/index.tsx
+++ src/components/MovieBanner/index.tsx
@@ -1,20 +1,35 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
 import { Movie } from '../../interfaces/movie.interface'
 import './MovieBanner.css'
+import { Pagination, Navigation } from 'swiper/modules'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 
 export default function MovieBanner({ movies }: { movies: Movie[] }) {
 	return (
 		<>
-			{movies.map((movie: Movie) => (
-				<div
-					className='movie-banner'
-					style={{
-						backgroundImage: `url('https://image.tmdb.org/t/p/w500${movie.backdrop_path}')`,
-					}}
-					key={movie.title}
-				>
-					<h2 className='movie-banner-title'>{movie.title}</h2>
-				</div>
-			))}
+			<Swiper
+				slidesPerView={1}
+				spaceBetween={30}
+				loop={true}
+				navigation={true}
+				modules={[Pagination, Navigation]}
+				className='mySwiper'
+			>
+				{movies.map((movie: Movie) => (
+					<SwiperSlide
+						className='movie-banner'
+						style={{
+							backgroundImage: `url('https://image.tmdb.org/t/p/w500${movie.backdrop_path}')`,
+						}}
+						key={movie.title}
+					>
+						<h2 className='movie-banner-title'>{movie.title}</h2>
+					</SwiperSlide>
+				))}
+			</Swiper>
 		</>
 	)
 }
