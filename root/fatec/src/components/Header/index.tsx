6e85f7d8b40599b3a3cdcaca4d5bcfd8053b95df
Antonio Sabino <aslimajr@outlook.com>
2024-06-14 23:42:02 -0300

    feat: add logout in header component

--- root/fatec/src/components/Header/index.tsx
+++ root/fatec/src/components/Header/index.tsx
@@ -1,29 +1,37 @@
-import "./Header.css";
-import { Link } from "react-router-dom";
-import logo from "./../../assets/logo2.png.png";
+import './Header.css'
+import { Link } from 'react-router-dom'
+import logo from './../../assets/logo2.png.png'
 
 export default function Header() {
-  return (
-    <header className="header">
-      <div className="header-container">
-        <nav>
-          <ul>
-            <li>
-              <img src={logo} alt="logo" className="logo" />
-            </li>
-            <li>
-              <Link to="/">Início</Link>
-            </li>
-            <li>
-              <Link to="/favorites">Meus favoritos</Link>
-            </li>
-          </ul>
-        </nav>
-        <img
-          className="user"
-          src="https://cdn-icons-png.freepik.com/512/6915/6915987.png"
-        />
-      </div>
-    </header>
-  );
+	return (
+		<header className='header'>
+			<div className='header-container'>
+				<nav>
+					<ul>
+						<li>
+							<img
+								src={logo}
+								alt='logo'
+								className='logo'
+							/>
+						</li>
+						<li>
+							<Link to='/'>Início</Link>
+						</li>
+						<li>
+							<Link to='/favorites'>Meus favoritos</Link>
+						</li>
+					</ul>
+				</nav>
+				<img
+					className='logout'
+					src='img/logout.png'
+					onClick={() => {
+						localStorage.clear()
+						window.location.reload()
+					}}
+				/>
+			</div>
+		</header>
+	)
 }
